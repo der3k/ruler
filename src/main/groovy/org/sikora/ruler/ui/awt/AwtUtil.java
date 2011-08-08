502f7126ce91ef90db1035544d36c31c81e1e2a7
Richard Sikora <richard.sikora@gmail.com>
2011-08-08 16:38:22 +0200

    #19 added javadoc to AwtUtil

--- src/main/groovy/org/sikora/ruler/ui/awt/AwtUtil.java
+++ src/main/groovy/org/sikora/ruler/ui/awt/AwtUtil.java
@@ -7,26 +7,51 @@ import javax.swing.text.JTextComponent;
 import java.awt.*;
 
 /**
- * User: der3k
- * Date: 16.7.11
- * Time: 11:19
+ * AWT utility functions.
  */
-class AwtUtil {
+public class AwtUtil {
+
+  /**
+   * Sets location on size to a window.
+   *
+   * @param window window that would be updated
+   * @param x      x position
+   * @param y      y position
+   * @param dx     width
+   * @param dy     height
+   */
   static void setLocationAndSize(final Window window, final int x, final int y, final int dx, final int dy) {
     window.setLocation(x, y);
     window.setSize(dx, dy);
   }
 
+  /**
+   * Sets background and foreground color together with font size to a text component.
+   *
+   * @param field    text component that would be updated
+   * @param fontSize font size
+   */
   public static void setFontAndColor(final JTextComponent field, final int fontSize) {
     field.setBackground(Color.BLACK);
     field.setForeground(Color.YELLOW);
     field.setFont(new Font("Candara", Font.PLAIN, fontSize));
   }
 
+  /**
+   * Sets empty border of given size to a text component.
+   *
+   * @param field text component that would be updated
+   * @param size  border size in pixels
+   */
   public static void setEmptyBorder(final JTextComponent field, final int size) {
     field.setBorder(BorderFactory.createEmptyBorder(size, size, size, size));
   }
 
+  /**
+   * Makes a window opaque.
+   *
+   * @param window window that would be updated
+   */
   public static void makeWindowOpaque(final Window window) {
     if (window instanceof JWindow) {
       JWindow jWindow = ((JWindow) window);
