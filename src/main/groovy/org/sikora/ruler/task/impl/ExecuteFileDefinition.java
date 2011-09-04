2e876bcda4d618b74ea75c27ffd7864f1e834385
der3k <rs.pipe@gmail.com>
2011-09-04 18:36:41 +0200

    implemented execute file task definition

--- src/main/groovy/org/sikora/ruler/task/impl/ExecuteFileDefinition.java
+++ src/main/groovy/org/sikora/ruler/task/impl/ExecuteFileDefinition.java
@@ -1,26 +1,16 @@
 package org.sikora.ruler.task.impl;
 
-import com.sun.jna.Native;
-import com.sun.jna.Pointer;
-import com.sun.jna.WString;
-import com.sun.jna.platform.win32.Kernel32;
-import com.sun.jna.platform.win32.WinBase;
-import com.sun.jna.platform.win32.WinDef;
-import com.sun.jna.win32.StdCallLibrary;
 import org.sikora.ruler.context.InputEventInContext;
 import org.sikora.ruler.model.input.InputDriver;
 import org.sikora.ruler.task.Result;
 import org.sikora.ruler.task.Task;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 
 import static org.sikora.ruler.model.input.InputDriver.Command.*;
 
-/**
- * User: der3k
- * Date: 2.9.11
- * Time: 23:45
- */
 public class ExecuteFileDefinition extends SimpleDefinition {
   private final File file;
 
@@ -39,47 +29,20 @@ public class ExecuteFileDefinition extends SimpleDefinition {
           public void display() {
             event.inputDriver().issue(InputDriver.InputCommand.of(HIDE));
             event.inputDriver().issue(InputDriver.InputCommand.of(RESET));
-            event.resultWindow().display(name());
+            try {
+              final ArrayList<String> cmd = new ArrayList<String>();
+              cmd.add("rundll32.exe");
+              cmd.add("shell32.dll,ShellExec_RunDLL");
+              cmd.add(file.getCanonicalPath());
+              final ProcessBuilder processBuilder = new ProcessBuilder(cmd);
+              processBuilder.start();
+            } catch (IOException e) {
+              throw new RuntimeException(e);
+            }
 
-//            WinBase.STARTUPINFO startup = new WinBase.STARTUPINFO();
-//            startup.dwFlags = WinBase.STARTF_USESHOWWINDOW;
-//            startup.wShowWindow = new WinDef.WORD(5L);
-//            WinBase.PROCESS_INFORMATION process = new WinBase.PROCESS_INFORMATION();
-//            //def cmd = 'c:\\windows\\system32\\cmd.exe /c start C:\\home\\etc\\links\\groovyconsole.lnk'
-//            String cmd = "c:\\windows\\explorer.exe";
-//            Kernel32Process lib = Kernel32Process.INSTANCE;
-//            boolean result = lib.CreateProcessW(null,
-//                new WString(cmd),
-//                null,
-//                null,
-//                false,
-//                WinBase.DETACHED_PROCESS,
-//                null,
-//                new WString("c:\\windows"),
-//                startup,
-//                process);
-//
-//            Kernel32.INSTANCE.GetLastError();
           }
         };
       }
     };
   }
-
-  interface Kernel32Process extends StdCallLibrary {
-    Kernel32Process INSTANCE = (Kernel32Process) Native.loadLibrary("kernel32", Kernel32Process.class);
-
-    boolean CreateProcessW(WString lpApplicationName,
-                           WString lpCommandLine,
-                           Pointer lpProcessAttributes,
-                           Pointer lpThreadAttributes,
-                           boolean bInheritHandles,
-                           long dwCreationFlags,
-                           Pointer lpEnvironment,
-                           WString lpCurrentDirectory,
-                           WinBase.STARTUPINFO lpStartupInfo,
-                           WinBase.PROCESS_INFORMATION lpProcessInformation
-    );
-  }
-
 }
